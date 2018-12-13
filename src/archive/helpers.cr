3b3810465fe4136dfd929468ff2ab984aa99888c
Omar Roth <omarroth@hotmail.com>
2018-12-12 19:09:31 -0600

    Remove db attribute from worker config

--- src/archive/helpers.cr
+++ src/archive/helpers.cr
@@ -14,13 +14,6 @@ end
 
 class WorkerConfig
   YAML.mapping({
-    db: NamedTuple(
-      user: String,
-      password: String,
-      host: String,
-      port: Int32,
-      dbname: String,
-    ),
     access_key: String,
     secret_key: String,
     batch_url:  String,
