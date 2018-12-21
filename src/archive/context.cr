6959f2e40cded81b0b8468c9372eb1312b8a959e
Omar Roth <omarroth@hotmail.com>
2018-12-21 12:23:31 -0600

    Monkey-patch HTTP::Server to provide remote_address

---
+++ src/archive/context.cr
@@ -0,0 +1,16 @@
+class HTTP::Server
+  # Instances of this class are passed to an `HTTP::Server` handler.
+  class NewContext < Context
+    # The `HTTP::Request` to process.
+    getter request : Request
+
+    # The `HTTP::Server::Response` to configure and write to.
+    getter response : Response
+
+    getter remote_address : Socket::IPAddress
+
+    # :nodoc:
+    def initialize(@request : Request, @response : Response, @remote_address : Socket::IPAddress)
+    end
+  end
+end
