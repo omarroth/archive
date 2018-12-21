589a90120d0e6f54386ece64e9e9dcfe729fee8d
Omar Roth <omarroth@hotmail.com>
2018-12-21 09:48:19 -0600

    Update archive master

--- src/archive.cr
+++ src/archive.cr
@@ -31,33 +31,16 @@ PG_URL = URI.new(
   path: CONFIG.db[:dbname],
 )
 
-PG_DB      = DB.open PG_URL
-BATCH_SIZE = 10000
+PG_DB = DB.open PG_URL
 
-begin
-  PG_DB.exec("BEGIN WORK")
-  PG_DB.exec("DECLARE C CURSOR FOR SELECT id FROM videos")
+get "/batch" do |env|
+  env.response.content_type = "application/json"
 
-  index = 0
-  get "/batch" do |env|
-    size = env.params.query["size"]?.try &.to_i?
-    size ||= BATCH_SIZE
+  start_ctid, end_ctid = PG_DB.query_one("SELECT start_ctid, end_ctid FROM batches WHERE finished = false ORDER BY RANDOM() LIMIT 1", as: {String, String})
+  response = PG_DB.query_all("SELECT id FROM videos WHERE ctid >= $1 AND ctid <= $2", start_ctid, end_ctid, as: String)
 
-    env.response.content_type = "application/json"
-
-    response = PG_DB.query_all("FETCH #{size} FROM C", as: String)
-    index += response.size
-
-    response.to_json
-  end
-
-  get "/current_index" do |env|
-    env.response.content_type = "application/json"
-    {"index" => index}.to_json
-  end
-
-  gzip true
-  Kemal.run
-ensure
-  PG_DB.exec("COMMIT WORK")
+  response.to_json
 end
+
+gzip true
+Kemal.run
