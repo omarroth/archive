eb668dbd225c6d85e04fe94398b1ce77c0cd6732
Omar Roth <omarroth@hotmail.com>
2018-12-23 00:35:06 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,3 +5,5 @@ createdb archive
 psql -c "CREATE USER kemal WITH PASSWORD 'kemal';"
 psql archive < config/sql/channels.sql
 psql archive < config/sql/videos.sql
+psql archive < config/sql/workers.sql
+psql archive < config/sql/batches.sql
